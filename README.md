6d89fa0ab352ac92a84ddc3a6cf4de9990888b1f
ilyasakbergen <ilijas9813@gmail.com>
2026-01-22 16:41:52 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sorted Linked List Library
+
+### Test task progress logs  
+[Progress Logs](./PROGRESS_LOG.md)
---
+++ tests/SortedLinkedListTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ilyasakbergen\SortedLinkedList\Tests;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class SortedLinkedListTest extends TestCase
+{
+    #[Test]
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
